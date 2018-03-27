0fb3e89d5e8ef789e5e1a409d750ada5519b2e58
navanga <navanga@gmail.com>
2018-03-27 23:54:11 +1100

    Bare bones server

--- src/main/java/com/navanga/altar/altardiscoveryserver/AltarDiscoveryServerApplication.java
+++ src/main/java/com/navanga/altar/altardiscoveryserver/AltarDiscoveryServerApplication.java
@@ -2,7 +2,10 @@ package com.navanga.altar.altardiscoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class AltarDiscoveryServerApplication {
 
