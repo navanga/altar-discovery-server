5bfb5125ac0b5fae42f6efcc74ad55068086a151
navanga <navanga@gmail.com>
2018-03-27 23:36:42 +1100

    Initial commit

---
+++ src/main/java/com/navanga/altar/altardiscoveryserver/AltarDiscoveryServerApplication.java
@@ -0,0 +1,12 @@
+package com.navanga.altar.altardiscoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltarDiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltarDiscoveryServerApplication.class, args);
+	}
+}
